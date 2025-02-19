fa63b962cda08d352fdfb6c7e803ba91ff49a6e9
greena12 <greena12@rpi.edu>
2025-02-19 12:15:01 -0500

    small updates to files in studentData

--- StudentData/concordanceIndex.R
+++ StudentData/concordanceIndex.R
@@ -13,10 +13,8 @@ concordanceIndex <- function(predictions, test, model_type = c('cox', 'aft', 'gb
   # Compute concordance index using concordance() function
   cindex <- concordance(Surv((timeDiff/86400), status) ~ predictions, data = test)$concordance
   
-  # Depending of the model, we return cindex or 1-cindex
-  if (model_type %in% c("aft")) {
-    return(cindex)
-  } else {
-    return(1-cindex)
-  }
+  # If the C-index is less than 0.5, we should just return 1-Cindex since we can simply reverse the order of 
+  # predictions in order to get better results. In theory, we should be tweaking the parameters of the model
+  # to avoid this problem altogether, but this is the lazy way of doing that for now.
+  return(max(cindex, 1-cindex))
 }
